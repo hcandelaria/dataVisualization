a9f871525e4b189b8fe270e47d31a1771bede9f3
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-07-30 19:40:31 -0400

    Added project links and rources.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,11 +8,18 @@ import {
   IonMenu,
   IonMenuToggle,
   IonNote,
-} from '@ionic/react';
+} from "@ionic/react";
 
-import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
-import './Menu.css';
+import { useLocation } from "react-router-dom";
+import {
+  analyticsOutline,
+  analyticsSharp,
+  logoGithub,
+  informationCircleOutline,
+  informationCircleSharp,
+  bookmarkOutline,
+} from "ionicons/icons";
+import "./Menu.css";
 
 interface AppPage {
   url: string;
@@ -23,44 +30,27 @@ interface AppPage {
 
 const appPages: AppPage[] = [
   {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
+    title: "Analysis",
+    url: "/page/analysis",
+    iosIcon: analyticsOutline,
+    mdIcon: analyticsSharp,
   },
-  {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
 ];
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+const links = [
+  {
+    title: "Repo",
+    url: "https://github.com/hcandelaria/health-insurance-coverage",
+    iosIcon: logoGithub,
+    mdIcon: logoGithub,
+  },
+  {
+    title: "Kaggle",
+    url: "https://www.kaggle.com/code/eamartey/health-insurance/data",
+    iosIcon: informationCircleOutline,
+    mdIcon: informationCircleSharp,
+  },
+];
 
 const Menu: React.FC = () => {
   const location = useLocation();
@@ -69,13 +59,25 @@ const Menu: React.FC = () => {
     <IonMenu contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
+          <IonListHeader>Health Insurance Coverage</IonListHeader>
+          {/* <IonNote>hi@ionicframework.com</IonNote> */}
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                <IonItem
+                  className={
+                    location.pathname === appPage.url ? "selected" : ""
+                  }
+                  routerLink={appPage.url}
+                  routerDirection="none"
+                  lines="none"
+                  detail={false}
+                >
+                  <IonIcon
+                    slot="start"
+                    ios={appPage.iosIcon}
+                    md={appPage.mdIcon}
+                  />
                   <IonLabel>{appPage.title}</IonLabel>
                 </IonItem>
               </IonMenuToggle>
@@ -84,12 +86,14 @@ const Menu: React.FC = () => {
         </IonList>
 
         <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
+          <IonListHeader>Sources</IonListHeader>
+          {links.map((link, index) => (
+            <a href={link.url} target="_blank">
+              <IonItem lines="none" key={index}>
+                <IonIcon slot="start" ios={link.iosIcon} md={link.mdIcon} />
+                <IonLabel>{link["title"]}</IonLabel>
+              </IonItem>
+            </a>
           ))}
         </IonList>
       </IonContent>
