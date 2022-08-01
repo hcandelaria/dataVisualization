6b5bc7f03a02cb1d50034cdf70c7838c3b6ada7a
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-08-01 00:43:49 -0400

    Updated BarGraph component for better css and usability.

--- src/components/BarGraph.tsx
+++ src/components/BarGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   BarChart,
   Bar,
@@ -9,71 +9,83 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-import { getHealthInsuranceData } from "../libs/api";
 import "./BarGraph.css";
-
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+} from "@ionic/react";
 interface BarGraphProps {
-  graphDataKey: graphDetails[];
-  data: State[];
+  title: string;
+  subtitle?: string;
+  graphDataKey: GraphDetails[];
+  payload: State[];
 }
 interface State {
   State: string;
-  "Average Monthly Tax Credit (2016)": string;
-  "Employer Health Insurance Coverage (2015)": Number;
-  "Health Insurance Coverage Change (2010-2015)": Number;
-  "Marketplace Health Insurance Coverage (2016)": Number;
-  "Marketplace Tax Credits (2016)": Number;
-  "Medicaid Enrollment (2013)": Number;
-  "Medicaid Enrollment (2016)": Number;
-  "Medicaid Enrollment Change (2013-2016)": Number;
-  "Medicare Enrollment (2016)": Number;
-  "State Medicaid Expansion (2016)": boolean;
-  "Uninsured Rate (2010)": Number;
-  "Uninsured Rate (2015)": Number;
-  "Uninsured Rate Change (2010-2015)": Number;
+  context: any
 }
 
-interface graphDetails {
+interface GraphDetails {
   color: string;
   dataKey: string;
 }
 
-const BarGraph: React.FC<BarGraphProps> = ({ data, graphDataKey }) => {
+const BarGraph: React.FC<BarGraphProps> = ({
+  payload,
+  graphDataKey,
+  title,
+  subtitle,
+}) => {
+  const [dataKeys] = useState<GraphDetails[]>(graphDataKey);
+  const [data] = useState<State[]>(payload);
+
+  useEffect(() => {
+  });
   return (
-    <>
-      <ResponsiveContainer width={"100%"} height={"100%"}>
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="State" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          {graphDataKey ? (
-            graphDataKey.map((info, index) => {
-              return (
-                <Bar
-                  key={index}
-                  dataKey={`${info["dataKey"]}`}
-                  fill={`${info["color"]}`}
-                />
-              );
-            })
-          ) : (
-            <></>
-          )}
-        </BarChart>
-      </ResponsiveContainer>
-    </>
+    <IonCard>
+      <IonCardHeader>
+        {subtitle ? <IonCardSubtitle>{subtitle}</IonCardSubtitle> : <></>}
+        <IonCardTitle>{title}</IonCardTitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+        <ResponsiveContainer width={"100%"} height={"100%"}>
+          <BarChart
+            data={data}
+            width={500}
+            height={300}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="State" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {dataKeys ? (
+              dataKeys.map((info, index) => {
+                return (
+                  <Bar
+                    key={index}
+                    dataKey={`${info["dataKey"]}`}
+                    fill={`${info["color"]}`}
+                  />
+                );
+              })
+            ) : (
+              <></>
+            )}
+          </BarChart>
+        </ResponsiveContainer>
+      </IonCardContent>
+    </IonCard>
   );
 };
 
