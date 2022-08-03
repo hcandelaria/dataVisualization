586e7fe1cd1ffcf116a7f4f2d03e9f78a8561805
hcandelaria <hcandelaria66.7@gmail.com>
2022-08-02 23:51:01 -0400

    Working on API to get health map geoJson and data to display.

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -5,8 +5,24 @@ import axios from "axios";
  * 
  * @return {*} 
  */
-export const getHealthInsuranceData = async () => {
+export const getHealthInsuranceData = async () => { 
     const URL = 'https://health-insurance-coverage.s3.amazonaws.com/data.json'
+    try {
+        const response = await axios.get(URL);
+        return response;
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+/**
+ * Get health information data from AWS S3
+ * 
+ * @return {*} 
+ */
+ export const getHealthInsuranceMapFeatures = async () => {
+    // const URL = 'https://health-insurance-coverage.s3.amazonaws.com/features.json'
+    const URL = 'https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json'
     try {
         const response = await axios.get(URL);
         return response;
